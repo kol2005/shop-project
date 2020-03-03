7b79cbcf6a4833b7a34caaa3b794c685eaa3baae
kol2005 <kol2005@naver.com>
2020-03-03 12:30:49 +0900

    2020-03-03

--- SpMVC_20_BBS_Final/src/main/java/com/biz/bbs/controller/BBsController.java
+++ SpMVC_20_BBS_Final/src/main/java/com/biz/bbs/controller/BBsController.java
@@ -3,6 +3,7 @@ package com.biz.bbs.controller;
 import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Qualifier;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -24,6 +25,7 @@ import lombok.extern.slf4j.Slf4j;
 public class BBsController {
 
 	@Autowired
+	@Qualifier("bbsV2")
 	private BBsService bbsService;
 	
 	@Autowired
