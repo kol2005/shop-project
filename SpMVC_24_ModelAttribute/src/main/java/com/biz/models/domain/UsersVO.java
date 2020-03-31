3e06be1df95a53b14b03abc4bdc286f52ce67632
kol2005 <kol2005@naver.com>
2020-03-31 16:40:46 +0900

    2020-03-31

---
+++ SpMVC_24_ModelAttribute/src/main/java/com/biz/models/domain/UsersVO.java
@@ -0,0 +1,22 @@
+package com.biz.models.domain;
+
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+import lombok.ToString;
+
+@Getter
+@Setter
+@AllArgsConstructor
+@NoArgsConstructor
+@ToString
+@Builder
+public class UsersVO {
+
+	private String userId;
+	private String password;
+	private String userName;
+	private String userRolle;
+}
