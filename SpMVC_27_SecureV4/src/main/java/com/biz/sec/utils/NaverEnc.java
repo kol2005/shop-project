b1403603c6eacf6b676c2332099a858c07f20c81
kol2005 <kol2005@naver.com>
2020-04-17 16:28:50 +0900

    2020-04-17

--- SpMVC_27_SecureV4/src/main/java/com/biz/sec/utils/NaverEnc.java
+++ SpMVC_27_SecureV4/src/main/java/com/biz/sec/utils/NaverEnc.java
@@ -11,7 +11,7 @@ import org.jasypt.encryption.pbe.StandardPBEStringEncryptor;
  * 키보드에서 문자열을 입력받아서
  * 암호화된 문자열을 생성
  */
-public class EncMain {
+public class NaverEnc {
 
 	public static void main(String[] args) {
 		
@@ -27,10 +27,10 @@ public class EncMain {
 		
 		Scanner scanner = new Scanner(System.in);
 		
-		System.out.print("MySQL UserName >>");
+		System.out.print("Naver UserName >>");
 		String userName = scanner.nextLine();
 		
-		System.out.print("MySQL Password >>");
+		System.out.print("Naver Password >>");
 		String password = scanner.nextLine();
 		
 		/*
@@ -46,10 +46,10 @@ public class EncMain {
 		System.out.printf("userName : %s \n", encUserName);
 		System.out.printf("password : %s \n", encPassword);
 		
-		String saveFile = "./src/main/webapp/WEB-INF/spring/db.connection.properties";
+		String saveFile = "./src/main/webapp/WEB-INF/spring/properties/naver.connection.properties";
 		
-		String saveUserName = String.format("mysql.username=ENC(%s)",encUserName);
-		String savePassword = String.format("mysql.password=ENC(%s)",encPassword);
+		String saveUserName = String.format("naver.username=ENC(%s)",encUserName);
+		String savePassword = String.format("naver.password=ENC(%s)",encPassword);
 		
 		try {
 			PrintWriter out = new PrintWriter(saveFile);
@@ -63,7 +63,7 @@ public class EncMain {
 		}
 		scanner.close();
 		
-		System.out.println("db.connection.properties 저장 완료!!!");
+		System.out.println("naver.connection.properties 저장 완료!!!");
 		
 	}
 
